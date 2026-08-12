8e2e1676c0668a458a11cb1f3f16e90e55015ed0
Siddik Imran Gadkari <gadakarisiddhik@gmail.com>
2026-08-13 00:03:10 +0530

    Added DSA O(n**2) Complexity Analysis Example

--- 02_Complexity_Analysis/O_n_Square.py
+++ 02_Complexity_Analysis/O_n_Square.py
@@ -7,7 +7,7 @@ def find_duplicate(numbers):
             if i != j  and numbers[i] == numbers[j]:
                 return True
     return False
-
+ 
 numbers = [10,2,35,22,48]
 result = find_duplicate(numbers)
 
