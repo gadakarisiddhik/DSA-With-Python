09b0ac394e0e960f175eb085ac2674144aa47027
Siddik Imran Gadkari <gadakarisiddhik@gmail.com>
2026-08-13 00:04:57 +0530

    Added DSA O(1) Complexity Analisis Example

--- 02_Complexity_Analysis/O_of_1.py
+++ 02_Complexity_Analysis/O_of_1.py
@@ -1,7 +1,6 @@
-#O(1)
+"""
+O(1), Your data is increse but time complexity is 1
+"""
 makrs = [88,72,95,61,79,90,22,1,0,55,66,98,78,5]
 fisrt_m = makrs[0]
 print(fisrt_m)
-"""
-Your data is increse but time complexity is 1
-"""
